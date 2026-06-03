42cad75ae5dfd39a8fbc82cdd05b502cf003b913
Perdyx <29049651+Perdyx@users.noreply.github.com>
2026-06-03 13:46:59 -0700

    Add main class

---
+++ Sentinel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+
+namespace Sentinel
+{
+    public class Sentinel : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded!", ConsoleColor.Yellow);
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log($"{Name} has been unloaded!", ConsoleColor.Yellow);
+        }
+    }
+}
